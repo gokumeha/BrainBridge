0eb95c2883947f4485d53f56a4c0086270de36ca
gokumeha <senhansenu@gmail.com>
2025-11-12 09:35:54 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      base: '/BrainBridge/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
@@ -21,3 +22,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
